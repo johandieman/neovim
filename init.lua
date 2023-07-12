e29b8ee6300f05e7046966692df4f0cdfe154738
johandieman <johanvdmerwelus@gmail.com>
2023-07-12 08:39:29 +0200

    changes

--- init.lua
+++ init.lua
@@ -330,6 +330,29 @@ require("nvim-tree").setup({
     dotfiles = true,
   },
 })
+
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+   vim.keymap.set('n', '<leader>m', ':NvimTreeFocus<CR>', {
+    noremap = true
+  })
+end
+
+-- pass to setup along with your other options
+require("nvim-tree").setup {
+  ---
+  on_attach = my_on_attach,
+  ---
+}
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
